a7ccc722a7e0b00649df6767aff449ebaac845c2
kate-del <chernyakova.c@gmail.com>
2022-03-28 23:19:29 +0100

    fix cookies

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://auth-group.herokuapp.com/"
-  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
